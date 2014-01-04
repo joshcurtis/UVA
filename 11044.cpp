427e690128925546fc9ea42e0b84ab5fc1f242f0
Josh Curtis <jcurtis825@gmail.com>
2014-01-04 00:16:11 -0800

    Solved 11044

---
+++ 11044.cpp
@@ -0,0 +1,18 @@
+/*
+Solutiont to 11044
+ */
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int cases;
+    cin  >> cases;
+    unsigned long long height, width;
+    while(cases--)
+    {
+	cin >> height >> width;
+	//Divisions have to happen first so that the result truncates
+	cout << (height / 3) * (width / 3) << endl;
+    }
+}
